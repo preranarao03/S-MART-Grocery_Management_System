5e5c8be73867854e6851231d90b7ca6b7fd212f4
preranarao03 <113436143+preranarao03@users.noreply.github.com>
2024-08-19 00:57:44 +0530

    Delete grocery directory

--- grocery/stock.php
+++
@@ -1,6 +0,0 @@
-<?php
-   $db="grocery";
-   $connect = mysqli_connect('localhost','root','',$db);
-   $query = mysqli_query($connect,"call stock()");
-   header('Location: Admin_logged.php');
-   ?>
